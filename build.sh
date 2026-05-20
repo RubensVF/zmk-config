b4d7d24d58bd4325fbaaf883fb189024cb8012d5
Rubens <rubenvelazquez244@gmail.com>
2026-05-20 01:07:48 -0600

    add FN keys & UPDATE SCRIPT

--- build.sh
+++ build.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
+
 BOARD="nice_nano/nrf52840"
 SHIELD="${1:-corne_left}"
 
 docker run --rm \
-  -v "$(pwd)/zmk:/zmk" \
-  -v "$(pwd)/zmk-config:/zmk-config" \
+  -u "$(id -u):$(id -g)" \
+  -v "$(pwd):/work" \
   zmkfirmware/zmk-build-arm:stable \
-  bash -c "cd /zmk && \
+  bash -c "cd /work/zmk && \
            west update && \
            west zephyr-export && \
-           rm -rf /zmk/build && \
+           rm -rf build && \
            west build -s app -b $BOARD -- \
-             -DZMK_CONFIG=/zmk-config/config \
+             -DZMK_CONFIG=/work/zmk-config/config \
              -DSHIELD=$SHIELD"
 
 if [ $? -eq 0 ]; then
   cp zmk/build/zephyr/zmk.uf2 ./${SHIELD}.uf2
-  echo "✅ Firmware listo: ./${SHIELD}.uf2"
+  echo "Firmware listo: ./${SHIELD}.uf2"
 else
-  echo "❌ Error en la compilación"
+  echo "Error en la compilación"
 fi
